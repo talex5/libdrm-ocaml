e7060499637536a5d2c448ca576c1c53b8e4fc13
Thomas Leonard <talex5@gmail.com>
2025-11-15 15:56:37 +0000

    Don't use a plane twice in example

--- examples/atomic.ml
+++ examples/atomic.ml
@@ -4,19 +4,23 @@ module K = Drm.Kms
 
 let println fmt = Fmt.pr (fmt ^^ "@.")
 
-let find_plane dev ~crtc_idx =
-  let compatible plane_id =
-    let plane = K.Plane.get dev plane_id in
-    if plane.possible_crtcs land (1 lsl crtc_idx) <> 0 then (
-      let props = K.Plane.get_properties dev plane_id in
-      match K.Properties.Values.get_value props K.Plane.typ with
-      | Some `Primary -> Some props.metadata
-      | _ -> None
-    ) else None
+let find_plane dev ~crtc_idx ~free_planes =
+  let rec aux = function
+    | [] -> Fmt.failwith "No suitable primary plane for CRTC with index %d" crtc_idx
+    | x :: xs ->
+      let plane = K.Plane.get dev x in
+      if plane.possible_crtcs land (1 lsl crtc_idx) <> 0 then (
+        let props = K.Plane.get_properties dev x in
+        match K.Properties.Values.get_value props K.Plane.typ with
+        | Some `Primary ->
+          free_planes := List.filter ((<>) x) !free_planes;
+          Some props.metadata
+        | _ -> aux xs
+      ) else aux xs
   in
-  K.Plane.list dev |> List.find_map compatible
+  aux !free_planes
 
-let show_test_page (t : Resources.t) rq (c : K.Connector.t) =
+let show_test_page ~free_planes (t : Resources.t) rq (c : K.Connector.t) =
   let obj = K.Connector.get_properties t.dev c.connector_id in
   match K.Properties.Values.get_value_exn obj K.Connector.crtc_id with
   | None -> Fmt.failwith "Connector %a has no CRTC" Drm.Id.pp c.connector_id
@@ -26,7 +30,7 @@ let show_test_page (t : Resources.t) rq (c : K.Connector.t) =
     let size = (mode.hdisplay, mode.vdisplay) in
     let crtc_idx = Resources.crtc_index t crtc_id in
     let plane =
-      match find_plane t.dev ~crtc_idx with
+      match find_plane t.dev ~crtc_idx ~free_planes with
       | Some x -> x
       | None ->
         Fmt.failwith "No suitable primary plane for CRTC %a (with index %d)"
@@ -54,8 +58,9 @@ let () =
   Drm.Client_cap.(set_exn atomic) t.dev true;
   let connected = List.filter Utils.is_connected t.connectors in
   println "Found %d connected connectors" (List.length connected);
+  let free_planes = ref (K.Plane.list t.dev) in
   let rq = K.Atomic_req.create () in
-  List.iter (show_test_page t rq) connected;
+  List.iter (show_test_page ~free_planes t rq) connected;
   println "Checking that commit will work...";
   match K.Atomic_req.commit ~test_only:true t.dev rq with
   | exception Unix.Unix_error (code, _, _) ->
